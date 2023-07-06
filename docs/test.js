54fca35ee19a611932eeec5b340d188d4574d1ea
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2023-07-06 12:22:53 -0400

    Create test.js

---
+++ docs/test.js
@@ -0,0 +1,51 @@
+/*
+(c) 2023 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+"use strict";
+
+const initPageTime = performance.now();
+
+const asyncWindow = new Promise(function (resolve, reject) {
+  window.addEventListener("load", function (evt) {
+    resolve(evt);
+  });
+});
+
+const asyncErrorLog = (async function () {
+  try {
+    const module = await import("https://scotwatson.github.io/Debug/20230705/ErrorLog.mjs");
+    return module;
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+const asyncQueue = (async function () {
+  try {
+    const module = await import("https://scotwatson.github.io/Containers/Test/Queue.mjs");
+    return module;
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+(async function () {
+  try {
+    const modules = await Promise.all( [ asyncWindow, asyncErrorLog, asyncQueue ] );
+    start(modules);
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+async function start( [ evtWindow, ErrorLog, Queue ] ) {
+  try {
+  } catch (e) {
+    ErrorLog.rethrow({
+      functionName: "start",
+      error: e,
+    });
+  }
+}
