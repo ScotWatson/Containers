f5c7ead6ca582e8481521213cab607530f2778e0
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2023-07-06 12:23:18 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,5 @@
 /*
-(c) 2022 Scot Watson  All Rights Reserved
+(c) 2023 Scot Watson  All Rights Reserved
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
@@ -15,7 +15,7 @@ const asyncWindow = new Promise(function (resolve, reject) {
 
 const asyncErrorLog = (async function () {
   try {
-    const module = await import("https://scotwatson.github.io/Debug/ErrorLog.mjs");
+    const module = await import("https://scotwatson.github.io/Debug/20230705/ErrorLog.mjs");
     return module;
   } catch (e) {
     console.error(e);
@@ -24,7 +24,7 @@ const asyncErrorLog = (async function () {
 
 const asyncQueue = (async function () {
   try {
-    const module = await import("https://scotwatson.github.io/Containers/Queue.mjs");
+    const module = await import("https://scotwatson.github.io/Containers/20230705/Queue.mjs");
     return module;
   } catch (e) {
     console.error(e);
